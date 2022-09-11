9f573d0aac47857b3693397ccc7a97bfe2878f43
ArtyomBeldeiko <artyom.beldeiko@gmail.com>
2022-09-11 22:32:05 +0300

    Initial Commit

---
+++ MPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MPlayer
+//
+//  Created by Artyom Beldeiko on 11.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
